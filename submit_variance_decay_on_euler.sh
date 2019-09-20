f6aea4ced8535451fd13c3efb5122edacd1f9930
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2019-09-20 10:32:21 +0200

    submitting for multiple orders

--- submit_variance_decay_on_euler.sh
+++ submit_variance_decay_on_euler.sh
@@ -6,52 +6,61 @@ function submit() {
     bsub -N -B -W 120:00 -R 'rusage[mem=128000]' "$@"
 }
 
-for variable in rho mx my E all;
+for order in 1 2
 do
-    submit python python/estimate_variance_decay.py \
-	--input_basename /cluster/work/math/klye/single_sample_structure_functions/experiments_mean_field/kh/p0_06/N{resolution}/kh_1.nc \
-	--starting_resolution 64 \
-	--max_resolution 1024 \
-	--title 'Kelvin-Helmholtz $\epsilon=0.06$' \
-	--variable ${variable}
-
-    submit python python/estimate_variance_decay.py \
-	--input_basename /cluster/work/math/klye/single_sample_structure_functions/experiments_mean_field/cloudshock/p0_03125/N{resolution}/cloudshock_1.nc \
-	--starting_resolution 64 \
-	--max_resolution 1024 \
-	--title 'Cloudshock $\epsilon=1/32$' \
-	--variable ${variable}
-
-    submit python python/estimate_variance_decay.py \
-	--input_basename /cluster/work/math/klye/single_sample_structure_functions/experiments_mean_field/rm/p0_06/N{resolution}/rm_1.nc \
-	--starting_resolution 64 \
-	--max_resolution 1024 \
-	--title 'Richtmeyer-Meshkov $\epsilon=0.06$' \
-	--variable ${variable}
-
-    submit python python/estimate_variance_decay.py \
-	--input_basename /cluster/work/math/klye/single_sample_structure_functions/experiments_mean_field/shockvortex/p0_06/N{resolution}/shockvortex_1.nc \
-	--starting_resolution 64 \
-	--max_resolution 1024 \
-	--title 'Shockvortex $\epsilon=0.06$' \
-	--variable ${variable}
-    
-
-    submit python python/estimate_variance_decay.py \
-	--input_basename /cluster/work/math/klye/single_sample_structure_functions/experiments_mean_field/kh/p0_06/N{resolution}/kh_1.nc \
-	--starting_resolution 64 \
-	--max_resolution 1024 \
-	--title 'Kelvin-Helmholtz $\epsilon=0.06$' \
-	--variable ${variable}
-
-    for H in 0.125 0.5 0.75
+    for variable in rho mx my E all;
     do
 	submit python python/estimate_variance_decay.py \
-	    --input_basename /cluster/work/math/klye/single_sample_structure_functions/experiments_mean_field/fbb/H${H//./_}/N{resolution}/euler_fbb_1.nc \
+	    --input_basename /cluster/work/math/klye/single_sample_structure_functions/experiments_mean_field/kh/p0_06/N{resolution}/kh_1.nc \
 	    --starting_resolution 64 \
 	    --max_resolution 1024 \
-	    --title "fBb \$H=${H}\$" \
-	    --variable ${variable}
+	    --title 'Kelvin-Helmholtz $\epsilon=0.06$' \
+	    --variable ${variable} \
+	    --norm_order ${order}
+	
+	submit python python/estimate_variance_decay.py \
+	    --input_basename /cluster/work/math/klye/single_sample_structure_functions/experiments_mean_field/cloudshock/p0_03125/N{resolution}/cloudshock_1.nc \
+	    --starting_resolution 64 \
+	    --max_resolution 1024 \
+	    --title 'Cloudshock $\epsilon=1/32$' \
+	    --variable ${variable} \
+	    --norm_order ${order}
+
+	submit python python/estimate_variance_decay.py \
+	    --input_basename /cluster/work/math/klye/single_sample_structure_functions/experiments_mean_field/rm/p0_06/N{resolution}/rm_1.nc \
+	    --starting_resolution 64 \
+	    --max_resolution 1024 \
+	    --title 'Richtmeyer-Meshkov $\epsilon=0.06$' \
+	    --variable ${variable} \
+	    --norm_order ${order}
+	
+	submit python python/estimate_variance_decay.py \
+	    --input_basename /cluster/work/math/klye/single_sample_structure_functions/experiments_mean_field/shockvortex/p0_06/N{resolution}/shockvortex_1.nc \
+	    --starting_resolution 64 \
+	    --max_resolution 1024 \
+	    --title 'Shockvortex $\epsilon=0.06$' \
+	    --variable ${variable} \
+	    --norm_order ${order}
+    
+
+	submit python python/estimate_variance_decay.py \
+	    --input_basename /cluster/work/math/klye/single_sample_structure_functions/experiments_mean_field/kh/p0_06/N{resolution}/kh_1.nc \
+	    --starting_resolution 64 \
+	    --max_resolution 1024 \
+	    --title 'Kelvin-Helmholtz $\epsilon=0.06$' \
+	    --variable ${variable} \
+	    --norm_order ${order}
+
+	for H in 0.125 0.5 0.75
+	do
+	    submit python python/estimate_variance_decay.py \
+		--input_basename /cluster/work/math/klye/single_sample_structure_functions/experiments_mean_field/fbb/H${H//./_}/N{resolution}/euler_fbb_1.nc \
+		--starting_resolution 64 \
+		--max_resolution 1024 \
+		--title "fBb \$H=${H}\$" \
+		--variable ${variable} \
+		--norm_order ${order}
+	done
     done
 done
 
