81e9eb83be5a8783aed7b193c769a0b9e8ab389e
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2019-10-17 20:08:31 +0200

    fixes

--- submit_variance_decay_on_euler.sh
+++ submit_variance_decay_on_euler.sh
@@ -14,13 +14,13 @@ do
     do
 	for functional in 'identity' 'm2';
 	do
-	    for time_averaged in '' '_time_integratedd';
+	    for time_averaged in '' '_time_integrated';
 	    do
 		# This looks a bit complicated, but basically, we will use the precomputed
 		# m2 functional if we are time integrating, otherwise, we will compute in
 		# on line.
 		functional_filename='identity'
-		functional_to_compute=functional
+		functional_to_compute=${functional}
 		if [[ "${functional}" == "m2" ]];
 		then
 		    if [[ "${time_averaged}" == "_time_integrated" ]]
@@ -45,16 +45,16 @@ do
 		
 		
 		submit python python/estimate_variance_decay.py \
-		       --input_basename ${datapath}experiments_full_time_average/kh/p0_06/N{resolution}/kh_functional_${time_averaged}_${functional_filename}_${file_time}.nc \
+		       --input_basename ${datapath}experiments_full_time_average/kh/p0_06/N{resolution}/kh_functional${time_averaged}_${functional_filename}_${file_time}.nc \
 		       --starting_resolution 64 \
 		       --max_resolution 1024 \
-		       --title "Kelvin-Helmholtz \$\epsilon=0.06\$ ${time_averaged_title}"
+		       --title "Kelvin-Helmholtz \$\epsilon=0.06\$ ${time_averaged_title}" \
 		       --variable ${variable} \
 		       --norm_order ${order} \
 		       --functional ${functional_to_compute}
 		
 		submit python python/estimate_variance_decay.py \
-		       --input_basename ${datapath}experiments_full_time_average/cloudshock/p0_06/N{resolution}/cloudshock_functional_${time_averaged}${functional_filename}_${file_time}.nc \
+		       --input_basename ${datapath}experiments_full_time_average/cloudshock/p0_06/N{resolution}/cloudshock_functional${time_averaged}_${functional_filename}_${file_time}.nc \
 		       --starting_resolution 64 \
 		       --max_resolution 1024 \
 		       --title "Cloudshock \$\\epsilon=0.06\$ ${time_averaged_title}" \
@@ -63,7 +63,7 @@ do
 		       --functional ${functional_to_compute}
 
 		submit python python/estimate_variance_decay.py \
-		       --input_basename ${datapath}experiments_full_time_average/rm/p0_06/N{resolution}/rm_functional_${time_averaged}${functional_filename}_${file_time}.nc \
+		       --input_basename ${datapath}experiments_full_time_average/rm/p0_06/N{resolution}/rm_functional${time_averaged}_${functional_filename}_${file_time}.nc \
 		       --starting_resolution 64 \
 		       --max_resolution 1024 \
 		       --title "Richtmeyer-Meshkov \$\\epsilon=0.06\$ ${time_averaged_title}" \
@@ -72,7 +72,7 @@ do
 		       --functional ${functional_to_compute}
 		
 		submit python python/estimate_variance_decay.py \
-		       --input_basename ${datapath}experiments_full_time_average/shockvortex/p0_06/N{resolution}/shockvortex_functional_${time_averaged}${functional_filename}_${file_time}.nc \
+		       --input_basename ${datapath}experiments_full_time_average/shockvortex/p0_06/N{resolution}/shockvortex_functional${time_averaged}_${functional_filename}_${file_time}.nc \
 		       --starting_resolution 64 \
 		       --max_resolution 1024 \
 		       --title "Shockvortex \$\epsilon=0.06\$ ${time_averaged_title}" \
@@ -85,7 +85,7 @@ do
 		for H in 0.125 0.5 0.75
 		do
 		    submit python python/estimate_variance_decay.py \
-			   --input_basename ${datapath}experiments_full_time_average/fbb/H${H//./_}/N{resolution}/euler_fbb_functional_${time_averaged}${functional_filename}_${file_time}.nc \
+			   --input_basename ${datapath}experiments_full_time_average/fbb/H${H//./_}/N{resolution}/euler_fbb_functional${time_averaged}_${functional_filename}_${file_time}.nc \
 			   --starting_resolution 64 \
 			   --max_resolution 1024 \
 			   --title "fBb \$H=${H}\$ ${time_averaged_title}" \
